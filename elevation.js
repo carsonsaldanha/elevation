374c8cf6cf3ad8beca66936e0d66718686131a9f
carsonsaldanha <68826492+carsonsaldanha@users.noreply.github.com>
2020-08-09 17:38:51 -0500

    Add files via upload

---
+++ elevation.js
@@ -0,0 +1,80 @@
+// Establishes global variables
+let title = document.getElementById("title");
+let locationButton = document.getElementById("locationButton");
+let coordinatesElem = document.getElementById("coordinates");
+let elevationElem = document.getElementById("elevation");
+
+title.style.display = "block";
+
+// Displays the location button if the user has not already granted the location permission
+if (typeof localStorage["authorizedGeoLocation"] == "undefined" || localStorage["authorizedGeoLocation"] == "0" ) {
+    locationButton.style.display = "block";
+// Else, proceeds with getting coordinates and elevation
+} else {
+    locationButton.style.display = "none";
+    getData();
+}
+
+// Adds a click event handler to the location button
+locationButton.addEventListener("click", function() {
+    // Checks if the browser supports the geolocation API
+    if (navigator.geolocation) {
+        // Requests location permission
+        navigator.geolocation.getCurrentPosition(success, error);
+        locationButton.style.display = "none";
+        getData();
+    // Else, displays message indicating the user's browser is not supported    
+    } else {
+        locationButton.style.display = "none";
+        coordinatesElem.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}, false);
+
+// Sets a value in local storage to to 1 if the location permission is granted
+function success() {
+    localStorage["authorizedGeoLocation"] = 1;
+}
+
+// Sets a value in local storage to to 0 if the location permission is denied
+function error() {
+    localStorage["authorizedGeoLocation"] = 0;
+}
+
+// Gets the coordinates and elevation all in one
+function getData() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // Gets the latitude and longitude coordinates and displays them to the user
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        coordinatesElem.innerHTML = "Latitude: " + latitude.toString().substring(0, 10) + "<br>Longitude: " + longitude.toString().substring(0, 10);
+        getElevation(latitude, longitude);
+    });
+}
+
+// Gets the elevation using the user's given latitude and longitude coordinates
+function getElevation(latitude, longitude) {
+    // Generates a HTTP GET request string for USGS' Elevation Point Query Service
+    let getRequest = "https://nationalmap.gov/epqs/pqs.php?x=" + longitude + "&y=" + latitude + "&units=Feet&output=json";
+    // Gets the JSON from the request using jQuery with the JSON stored in data
+    $.getJSON(getRequest, function(data) {
+        // Gets the elevation from the JSON
+        let elevation = data.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation;
+        // Tries the second API because USGS doesn't have elevation data for that location
+        if (elevation == "-1000000") {
+            // Generates a HTTP GET request string for Open Topo Data's API
+            getRequest = "https://api.opentopodata.org/v1/srtm90m?locations=" + latitude + "," + longitude;
+            $.getJSON(getRequest, function(data) {
+                // Gets the elevation from the JSON
+                elevation = data.results[0].elevation;
+                // Indicates if no elevation data is available for this location
+                if (elevation == "null" || elevation == "NaN") {
+                    elevationElem.innerHTML = "Unfortunately, elevation data is not available for your location.";
+                } else {
+                    elevationElem.innerHTML = elevation + " m";
+                }
+            });
+        } else {
+            elevationElem.innerHTML = elevation + " ft";
+        }
+    });
+}
